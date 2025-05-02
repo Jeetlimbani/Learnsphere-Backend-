34b1a61e5c29b6f711162d49a0875a759ddc5bb6
demo1 <limbanijeet8@gmail.com>
2025-05-02 16:55:30 +0530

    Successfully completed

--- middleware/auth1.js
+++ middleware/auth1.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 
-const SECRET_KEY = 'b5e8a29fe93002fe51503e544bc9f2aafc78d36f8986172dac42e7ccbb1f902b013be565d821d5e036ef81aed68d0fe064592b1eee46b2a3174c974ac8cceed7';
 
 const authenticateUser = (req, res, next) => {
   const token = req.cookies.authToken;
@@ -10,7 +9,8 @@ const authenticateUser = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, SECRET_KEY);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded)
     req.user = decoded;
     next();
   } catch (err) {
